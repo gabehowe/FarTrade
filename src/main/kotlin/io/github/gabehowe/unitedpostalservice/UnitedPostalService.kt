f544eb830dc37b521d7c9044cff709f7b046740b
gabehowe <TheFluff1@>
2021-01-14 19:25:36 -0500

    initial commit

---
+++ src/main/kotlin/io/github/gabehowe/unitedpostalservice/UnitedPostalService.kt
@@ -0,0 +1,13 @@
+package io.github.gabehowe.univtedpostalservice
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class UnitedPostalService : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
